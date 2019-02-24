bcb3c7ebe86e0175a3f0425da8cd47e4d382417d
Kye Russell <kye.russell@thecut.net.au>
2019-02-24 19:56:44 +0800

    Cleaned up unused files / settings.

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,25 +12,6 @@ TIMEZONE = 'Australia/Perth'
 
 DEFAULT_LANG = 'en'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 3
-
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
