6dbd08cf917c03f539c909abb8038d01ac22fced
Kye Russell <kye.russell@thecut.net.au>
2019-02-27 20:31:29 +0800

    Added GA tags and sitemap.

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+import os
 
 AUTHOR = 'Kye Russell'
 SITENAME = 'Kye Russell'
-SITEURL = ''
+SITEURL = 'https://www.kye.id.au'
 
 PATH = 'content'
 
@@ -12,13 +10,20 @@ TIMEZONE = 'Australia/Perth'
 
 DEFAULT_LANG = 'en'
 
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['sitemap']
+
+
 # Customise URLs
+
 ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
-ARTICLE_URL = 'blog/{slug}'
+ARTICLE_URL = 'blog/{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
-PAGE_URL = '{slug}'
+PAGE_URL = '{slug}/'
+
 
 # Disable unused pages.
+
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -38,4 +43,29 @@ TAGS_SAVE_AS = ''
 CATEGORIES_SAVE_AS = ''
 AUTHORS_SAVE_AS = ''
 
+
+# Theme
+
 THEME = 'theme'
+
+
+# Sitemap
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 1.0,
+        'pages': 0.9,
+        'indexes': 0.8,
+    },
+    'changefreqs': {
+        'articles': 'weekly',
+        'pages': 'weekly',
+        'indexes': 'weekly'
+    },
+}
+
+ENVIRONMENT = os.environ.get('PELICAN_ENVIRONMENT', 'development')
+
+if ENVIRONMENT == 'development':
+    RELATIVE_URLS = True
